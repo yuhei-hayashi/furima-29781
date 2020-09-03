650ec865cdf8ef0ffe59f1c526361a9bfc529b54
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-03 12:54:05 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["furima_BASIC_AUTH_USER"] && password == ENV["furima_BASIC_AUTH_PASSWORD"]    end
+  end
 end
