9f0e12e50dd1f26dc81ee3f681b877887379b76e
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-04 15:46:46 +0900

    各種アクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+
+  def index
+  end
+  def new
+  end
+  def create
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end 
 end
