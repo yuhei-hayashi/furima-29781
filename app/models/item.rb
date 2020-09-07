3b2c6f8833d4fd14991f6e4c726aafe668fd287e
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-07 15:47:08 +0900

    Active_Hashによる各種テーブルの作成
    genre,status,delivery,area,day

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :day
+  belongs_to_active_hash :area
   belongs_to :user
   has_many :order
   has_one_attached :image
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_033452) do
+ActiveRecord::Schema.define(version: 2020_09_07_062602) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_033452) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
