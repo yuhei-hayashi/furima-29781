df0138694bd6b5f517ff0875d463cb249f3e0f6e
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-07 11:38:53 +0900

    バリデーション名　定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'is invalid. Input Input both letters and numbers.' }
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
+    PASSWORD_REGEX = validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'is invalid. Input Input both letters and numbers.' }
+    NAME_REGEX = with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
       validates :first_name
       validates :last_name
     end
-    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.'} do
+    NAME_READING_REGEX = with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.'} do
       validates :first_name_reading
       validates :last_name_reading
     end
