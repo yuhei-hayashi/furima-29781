d3039b4540dbe8aeeabda2d58e5cc49b880d9528
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-08 18:11:13 +0900

    商品詳細画面のフロントエンド実装

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,7 +2,7 @@ class Day < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
     { id: 1, name: '１〜２日で発送' },
-    { id: 2, name: '２〜３日で発送'},
+    { id: 2, name: '２〜３日で発送' },
     { id: 3, name: '４〜７日で発送'}
   ]
 end
