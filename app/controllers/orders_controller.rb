00eca300881e352380d0714687bb8e604cd1d6c7
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-09 13:04:02 +0900

    token作成の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,4 +15,6 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:post_code , :prefecture , :city ,:house_number , :building , :telephone).merge(user_id: current_user.id , item_id: params[:item_id])
   end
+
+  
 end
