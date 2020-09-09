b1241324c18007153b3ca9895528fdaa7f130b7d
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-09-09 12:35:21 +0900

    order#createの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,15 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = OrderAddress.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def order_params
+    params.require(:order_address).permit(:post_code , :prefecture , :city ,:house_number , :building , :telephone).merge(user_id: current_user.id , item_id: params[:item_id])
   end
 end
