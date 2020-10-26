bfb84ccb6055bd51a0e3d02a2c813eeddb338bbb
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-10-26 13:49:49 +0900

    herokupush用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
-  end
+  end 
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
